bca782489f228f140ab5e64b478b39d931f6d3bc
Lucie <debrabanderelucie@gmail.com>
2024-04-26 16:53:04 +0200

    display leaflet map

---
+++ app/javascript/discovered_map.js
@@ -0,0 +1,13 @@
+//Custom JS for map
+class DiscoveredMap {
+    constructor() {}
+}
+var map = L.map('disco-map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
