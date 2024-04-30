d6ef8c370e8943c9c93e35f992326fe9ff87e544
Lucie <debrabanderelucie@gmail.com>
2024-04-30 18:10:16 +0200

    use api data for map

--- app/javascript/discovered_map.js
+++ app/javascript/discovered_map.js
@@ -1,13 +1,21 @@
 //Custom JS for map
 class DiscoveredMap {
-    constructor() {}
+    constructor(map_data) {
+        this.map_data = map_data;
+        this.initializeMap();
+    }
+
+    initializeMap() {        
+        var map = L.map('disco-map').setView([48.86666, 2.33333], 2);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+
+        Object.keys(this.map_data).forEach(key => {
+            L.marker([this.map_data[key]['lat'], this.map_data[key]['lon']]).addTo(map)
+                .bindPopup(key)
+                .openPopup();
+        })
+    }
 }
-var map = L.map('disco-map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
