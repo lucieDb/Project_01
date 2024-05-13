946ade5807d67e510bc971ea9b6b78b4a4ddea96
Lucie <debrabanderelucie@gmail.com>
2024-05-13 17:21:48 +0200

    display new data

--- app/javascript/discovered_map.js
+++ app/javascript/discovered_map.js
@@ -12,10 +12,24 @@ class DiscoveredMap {
             attribution: '© OpenStreetMap contributors'
         }).addTo(map);
 
+        let that = this;
+
         Object.keys(this.map_data).forEach(key => {
-            L.marker([this.map_data[key]['lat'], this.map_data[key]['lon']]).addTo(map)
-                .bindPopup(key)
-                .openPopup();
-        })
-    }
-}
+            let marker = L.marker([this.map_data[key]['coord']['lat'], this.map_data[key]['coord']['lon']]).addTo(map)
+                .bindPopup(key);
+            marker.on('click', function() {
+                let cityName = key;
+                let temp = that.map_data[key]['temp'];
+                let feeling = that.map_data[key]['feeling'];
+                let wind = that.map_data[key]['wind'];
+
+                $("#info-city").html(`
+                    <h2>${cityName}</h2>
+                    <p>Temp : ${temp} °C</p>
+                    <p>Feeling : ${feeling} °C</p>
+                    <p>Wind speed : ${wind} km/h</p>
+                `);
+            });
+        });
+    };
+};
