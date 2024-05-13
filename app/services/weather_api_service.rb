a37864038748009b8e32fdd953a81672844c41e0
Lucie <debrabanderelucie@gmail.com>
2024-05-13 16:33:42 +0200

    add more info par city

--- app/services/weather_api_service.rb
+++ app/services/weather_api_service.rb
@@ -20,9 +20,18 @@ class WeatherApiService
         data_per_city = {}
         @cities.each do |city|
             city_data = client.current_weather(city: city)
-            data_per_city[city_data.name] = city_data.coord
+            data_per_city[city_data.name] = {
+                'coord' => city_data.coord,
+                'temp' => transform_kelvin_to_celsius(city_data.main['temp']),
+                'feeling' => transform_kelvin_to_celsius(city_data.main['feels_like']),
+                'wind' => city_data.wind['speed']
+            }
         end
         data_per_city
+    end
+
+    def transform_kelvin_to_celsius(kelvin)
+        (kelvin - 273.15).round(1)
     end
 
     def call_api
