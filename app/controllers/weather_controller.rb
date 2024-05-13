48ec9adf8ae41ab12a063ec135da8d50c7d97c86
Lucie <debrabanderelucie@gmail.com>
2024-05-13 16:33:18 +0200

    add new cities

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,7 +2,7 @@ require_relative '../.api_key.rb'
 
 class WeatherController < ApplicationController
 
-  CITIES_VISITED = %w(paris london tokyo amsterdam porto ljubljana rome praslin denpasar athens heraklion )
+  CITIES_VISITED = %w(paris london tokyo amsterdam porto ljubljana stockholm hongkong bruxelles naples venise seychelles denpasar athens heraklion paros)
 
   def index
     @data = WeatherApiService.perform(CITIES_VISITED)
